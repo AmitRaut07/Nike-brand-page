704c2cc0fe318b639229b5141172d3f3f73da5af
Amit Raut <153617048+AmitRaut07@users.noreply.github.com>
2025-05-24 15:59:43 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nike-brand-page/', 
+  base: '/Nike-brand-page/',  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
